891e5aed14a2ab5ec5ac26ff912837ba64902754
Jennifer <jenniferzevallos11@gmail.com>
2023-08-28 22:46:16 -0500

    primer commit

---
+++ LAB2. EJEMP09.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_2.Mayor_menor_prom09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tres números: ");
+
+            Console.Write("Ingrese primer número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese tercer número: ");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+
+            double mayor = num1;
+
+            if (num2 > mayor)
+            {
+                mayor = num2;
+            }
+           
+            if (num3 > mayor)
+            {
+                mayor = num3;
+            }
+
+            Console.WriteLine("El mayor número es: " + mayor);
+            Console.ReadKey();  
+
+        }
+    }
+}
---
+++ LAB2.EJEMP10.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Descuentos.prom10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double MConsumo;
+            double descuento = 0;
+            double impuesto;
+            double subtotal;
+            double total;
+
+            Console.Write("Ingrese el monto del consumo: ");
+            MConsumo = Convert.ToDouble(Console.ReadLine());
+
+            if (MConsumo <= 100.00)
+            {
+                descuento = MConsumo * 0.10;
+            }
+            else
+            {
+                descuento = MConsumo * 0.20;
+            }
+
+            impuesto = MConsumo * 0.18;
+            subtotal = MConsumo - descuento;
+            total = subtotal + impuesto;
+
+            Console.WriteLine("Monto del descuento: S/ " + descuento.ToString("0.00"));
+            Console.WriteLine("Impuesto: S/ " + impuesto.ToString("0.00"));
+            Console.WriteLine("Subtotal: S/ " + subtotal.ToString("0.00"));
+            Console.WriteLine("Importe a pagar: S/ " +  total.ToString("0.00"));
+            Console.ReadKey();  
+        }
+    }
+}
